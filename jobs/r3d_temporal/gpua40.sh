90aec8b1c9bae014722fcf37549e3fdc12e9e958
linusjuni <linus@juni.dk>
2025-12-03 09:07:21 +0100

    jobs

--- jobs/r3d_temporal/gpua40.sh
+++ jobs/r3d_temporal/gpua40.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#BSUB -q gpul40s
+#BSUB -q gpua40
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -J train_r3d_temporal_l40s
+#BSUB -J train_r3d_temporal_a40
 #BSUB -n 8
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=16GB]"
 #BSUB -W 12:00
-#BSUB -o outputs/output_r3d_temporal_l40s_%J.out
-#BSUB -e outputs/output_r3d_temporal_l40s_%J.err
+#BSUB -o outputs/output_r3d_temporal_a40_%J.out
+#BSUB -e outputs/output_r3d_temporal_a40_%J.err
 
 cd ~/projects/perimiter-net
 mkdir -p outputs
